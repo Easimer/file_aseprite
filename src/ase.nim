d2816228d3715b48be6a7cefa12dc28b0a009045
Daniel Meszaros <easimer@gmail.com>
2019-10-13 11:43:13 +0200

    Fix alpha channel when rasterizing

--- src/ase.nim
+++ src/ase.nim
@@ -35,38 +35,8 @@ proc loadSprite*(path: string): AsepriteImage =
   for frameIdx in 0 .. hdr.frames - 1:
     result.frames.add(readFrame(stream, hdr))
 
-proc alphaBlendOver(dst: var seq[uint8], src: seq[uint8], offDst: int, offSrc: int) =
-  let
-    aR = src[offSrc + 0].float / 255.0
-    aG = src[offSrc + 1].float / 255.0
-    aB = src[offSrc + 2].float / 255.0
-    aA = src[offSrc + 3].float / 255.0
-    bR = dst[offDst + 0].float / 255.0
-    bG = dst[offDst + 1].float / 255.0
-    bB = dst[offDst + 2].float / 255.0
-    bA = dst[offDst + 3].float / 255.0
-    
-  let denom = 1 / (aA + bA * (1 - aA))
-
-  let
-    outR = (aR + bR * (1 - aA)) * denom
-    outG = (aG + bG * (1 - aA)) * denom
-    outB = (aB + bB * (1 - aA)) * denom
-    outA = (aA + bA * (1 - aA)) * denom
-  
-  dst[offDst + 0] = cast[uint8](outR * 128)
-  dst[offDst + 1] = cast[uint8](outG * 128)
-  dst[offDst + 2] = cast[uint8](outB * 128)
-  dst[offDst + 3] = cast[uint8](outA * 128)
-
 proc numberOfFrames*(img: AsepriteImage): int = len(img.frames)
 proc numberOfLayers*(img: AsepriteImage, frame: int): int = len(img.frames[frame].layers)
-
-proc blendAlphas[T, U](a: T, b: U): uint8 =
-  var A: float = cast[float](a) / 255.0
-  var B: float = cast[float](a) / 255.0
-
-  result = cast[uint8](((A + B) / 2) * 255.0)
 
 proc rasterizeLayer*(img: AsepriteImage, frame: int, layerIndex: int): seq[uint8] =
   if frame >= 0 and frame < len(img.frames):
@@ -91,10 +61,11 @@ proc rasterizeLayer*(img: AsepriteImage, frame: int, layerIndex: int): seq[uint8
               let offX = cel.positionX + x
               let offBuffer = (offY * img.width + offX) * pixSize
               let offCel = (y * celWidth + x) * pixSize
-              #alphaBlendOver(result, cel.details.pixelData, offBuffer, offCel)
-              for b in 0..2:
+              for b in 0..3:
                 result[offBuffer + b] = cel.details.pixelData[offCel + b]
-              result[offBuffer + 3] = blendAlphas(cel.details.pixelData[offCel + 3], layer.opacity)
+              # TODO: This is fine if a layer only has one cel
+              if cel.details.pixelData[offCel + 3] != 0:
+                result[offBuffer + 3] = cast[uint8](layer.opacity)
       else:
         echo("layer is invisible")
     else:
