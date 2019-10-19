9c850a6e4e537c8916e6a9adabea5d2b9065d918
Daniel Meszaros <easimer@gmail.com>
2019-10-19 19:32:03 +0200

    Implement loading greyscale sprites

--- ase.nimble
+++ ase.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version       = "0.3.0"
+version       = "0.3.1"
 author        = "Daniel Meszaros"
 description   = "Loader for the Aseprite image format"
 license       = "LGPL-2.1"
--- src/ase.nim
+++ src/ase.nim
@@ -66,7 +66,7 @@ proc getFrameDuration*(img: AsepriteImage, frame: int): int =
   ## Returns the duration of a frame in milliseconds.
   img.frames[frame].duration
 
-proc rasterizeLayer*(img: AsepriteImage, frame: int, layerIndex: int): seq[uint8] =
+proc rasterizeLayerRGBA*(img: AsepriteImage, frame: int, layerIndex: int): seq[uint8] =
   if frame >= 0 and frame < len(img.frames):
     let pixSize = (img.depth div 8)
     result.setLen(img.width * img.height * pixSize)
@@ -87,10 +87,44 @@ proc rasterizeLayer*(img: AsepriteImage, frame: int, layerIndex: int): seq[uint8
               let offCel = (y * celWidth + x) * pixSize
               for b in 0..3:
                 result[offBuffer + b] = cel.details.pixelData[offCel + b]
-              # TODO: This is fine if a layer only has one cel
               if cel.details.pixelData[offCel + 3] != 0:
                 result[offBuffer + 3] = cast[uint8](img.layers[layerIndex].opacity)
     else:
       raise newException(IndexError, "Layer index is out of bounds!")
   else:
     raise newException(IndexError, "Frame index is out of bounds!")
+
+proc rasterizeLayerGreyscale*(img: AsepriteImage, frame: int, layerIndex: int): seq[uint8] =
+  if frame >= 0 and frame < len(img.frames):
+    let pixSize = 2
+    let outPixSize = 4
+    result.setLen(img.width * img.height * outPixSize)
+    let frame = img.frames[frame]
+    if layerIndex >= 0 and layerIndex < len(img.layers):
+      for cel in frame.cels:
+        if cel.layerIndex == layerIndex:
+          let imgHeight = img.height
+          let imgWidth = img.width
+          let celHeight = cel.details.height
+          let celWidth = cel.details.width
+
+          for y in 0 .. celHeight - 1:
+            for x in 0 .. celWidth - 1:
+              let offY = cel.positionY + y
+              let offX = cel.positionX + x
+              let offBuffer = (offY * img.width + offX) * outPixSize
+              let offCel = (y * celWidth + x) * pixSize
+              for b in 0..3:
+                result[offBuffer + b] = cel.details.pixelData[offCel + 0]
+              if cel.details.pixelData[offCel + 1] != 0:
+                result[offBuffer + 3] = cast[uint8](img.layers[layerIndex].opacity)
+    else:
+      raise newException(IndexError, "Layer index is out of bounds!")
+  else:
+    raise newException(IndexError, "Frame index is out of bounds!")
+
+proc rasterizeLayer*(img: AsepriteImage, frame: int, layerIndex: int): seq[uint8] =
+  case img.depth:
+    of 32: rasterizeLayerRGBA(img, frame, layerIndex)
+    of 16: rasterizeLayerGreyscale(img, frame, layerIndex)
+    else: raise newException(RangeError, "Pixel depth of " & $img.depth & " is not supported!")
